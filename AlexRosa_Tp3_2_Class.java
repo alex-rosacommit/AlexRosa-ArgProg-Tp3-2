6302fcd777a90387d6501f698f67e2e2a75d3200
Alex <alexrosa25@outlook.com>
2023-03-12 16:02:03 -0300

    Finalización ejercicio 2

---
+++ AlexRosa_Tp3_2_Class.java
@@ -0,0 +1,17 @@
+package alexRosa_Tp3_2;
+
+public class AlexRosa_Tp3_2_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String listaNombres = "Mateo_Martin_Romina_Lucas_Leo_Daniel_Mariana_Alejandro_Manuel_Carolina";
+		
+		String[]nombresEnArray = listaNombres.split("_");
+		
+		for(int index = 0; index < nombresEnArray.length; index++) {
+			System.out.println(nombresEnArray[index]);
+		}
+		
+	}
+
+}
